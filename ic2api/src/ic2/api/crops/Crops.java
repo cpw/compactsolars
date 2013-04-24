b86be8fcb7969b6eb6c73614337193279a712784
Christian <cpw@weeksfamily.ca>
2013-04-23 23:12:27 -0400

    Fix ic2api derp. RichardG!!!

--- ic2api/src/ic2/api/crops/Crops.java
+++ ic2api/src/ic2/api/crops/Crops.java
@@ -1,7 +1,5 @@
 package ic2.api.crops;
 
-import ic2.core.init.Localization;
-
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -19,63 +17,63 @@ import net.minecraftforge.oredict.OreDictionary;
  */
 public abstract class Crops {
 	public static Crops instance;
-	
+
 	/**
 	 * Adds a crop humidity and nutrient biome bonus.
-	 * 
+	 *
 	 * 0 indicates no bonus and negative values indicate a penalty.
-	 * 
+	 *
 	 * @param biome Biome to apply the bonus in
 	 * @param humidityBonus Humidity stat bonus
 	 * @param nutrientsBonus Nutrient stat bonus
 	 */
 	public abstract void addBiomeBonus(BiomeGenBase biome, int humidityBonus, int nutrientsBonus);
-	
+
 	/**
-	 * Gets the humidity bonus for a biome. 
-	 * 
+	 * Gets the humidity bonus for a biome.
+	 *
 	 * @param biome Biome to check
 	 * @return Humidity bonus or 0 if none
 	 */
 	public abstract int getHumidityBiomeBonus(BiomeGenBase biome);
-	
+
 	/**
-	 * Gets the nutrient bonus for a biome. 
-	 * 
+	 * Gets the nutrient bonus for a biome.
+	 *
 	 * @param biome Biome to check
 	 * @return Nutrient bonus or 0 if none
 	 */
 	public abstract int getNutrientBiomeBonus(BiomeGenBase biome);
-	
+
 	/**
 	 * Returns the list of registered crops.
-	 *  
+	 *
 	 * @return Registered crops by ID
 	 */
 	public abstract CropCard[] getCropList();
-	
+
 	/**
 	 * Auto-assign an ID to a plant and register it.
 	 * Usage of this method is not recommended! Other plants could take your IDs and cause your plants to turn into other plants.
-	 * 
+	 *
 	 * @param crop plant to register
 	 * @return The ID assigned to the plant
 	 */
 	public abstract short registerCrop(CropCard crop);
-	
+
 	/**
 	 * Attempt to register a plant to an ID.
 	 * If the ID is taken, the crop will not be registered and a console print will notify the user.
-	 * 
+	 *
 	 * @param crop plant to register
 	 * @param i ID to register the plant to
 	 * @return Whether the crop was registered
 	 */
 	public abstract boolean registerCrop(CropCard crop, int i);
-	
+
 	/**
 	 * Registers a base seed, an item used to plant a crop.
-	 * 
+	 *
 	 * @param stack item
 	 * @param id plant ID
 	 * @param size initial size
@@ -85,25 +83,25 @@ public abstract class Crops {
 	 * @return True if successful
 	 */
 	public abstract boolean registerBaseSeed(ItemStack stack, int id, int size, int growth, int gain, int resistance);
-	
+
 	/**
 	 * Finds a base seed from the given item.
-	 * 
+	 *
 	 * @return Base seed or null if none found
 	 */
 	public abstract BaseSeed getBaseSeed(ItemStack stack);
-	
+
 	/**
 	 * Execute registerSprites for all registered crop cards.
-	 * 
+	 *
 	 * This method will get called by IC2, don't call it yourself.
 	 */
 	@SideOnly(Side.CLIENT)
 	public abstract void startSpriteRegistration(IconRegister iconRegister);
-	
+
 	/**
 	 * Returns the ID for the given crop.
-	 * 
+	 *
 	 * @param crop Crop to look up
 	 * @return ID, or -1 if not found
 	 */
