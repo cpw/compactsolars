3667f1c598c1b368920757c688461ff286fd928b
Christian <cpw@weeksfamily.ca>
2013-08-22 13:59:08 -0400

    Update to 67.

--- ic2api/src/ic2/api/recipe/Recipes.java
+++ ic2api/src/ic2/api/recipe/Recipes.java
@@ -13,7 +13,9 @@ public class Recipes {
 	public static IMachineRecipeManager compressor;
 	public static IMachineRecipeManager centrifuge;
 	public static IMachineRecipeManager recycler;
-	public static IMachineRecipeManager metalformer;
+	public static IMachineRecipeManager metalformerextruding;
+	public static IMachineRecipeManager metalformercutting;
+	public static IMachineRecipeManager metalformerrolling;
 	public static IMachineRecipeManager orewasching;
 
 	/**
